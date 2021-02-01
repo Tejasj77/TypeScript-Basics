bec4b30802ffb2c914a80756110a09d09c751cb4
Tejasj77 <tejasjoshitj@gmail.com>
2021-02-01 22:01:55 +0530

    Adding basic TS Operations

---
+++ Basics/dist/type_compatibilty.js
@@ -0,0 +1,12 @@
+"use strict";
+var x;
+var y;
+var z = {
+    name: 'Fred',
+    lastname: 'Donovon',
+    age: 8000
+};
+x = { name: 'Tejas' };
+y = { name: 'David', age: 26 };
+x = y;
+console.log(x);
---
+++ Basics/src/type_compatibilty.ts
@@ -0,0 +1,11 @@
+let x:{name:string}
+let y:{name:string,age:number}
+let z:any = {
+    name:'Fred',
+    lastname:'Donovon',
+    age:8000
+}
+x={name:'Tejas'}
+y={name:'David',age:26}
+x=y
+console.log(x)
